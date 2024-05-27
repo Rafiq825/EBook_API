53036b22eb5e54122d738983c73856be8ead35c7
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-05-27 15:46:04 +0500

    modify

--- src/user/userRoute.ts
+++ src/user/userRoute.ts
@@ -3,7 +3,7 @@ import { createUser, loginUser } from "./userController";
 
 const userRouter = express.Router();
 
-// routes
+
 userRouter.post("/register", createUser);
 userRouter.post("/login", loginUser);
 
