fcec62b27a3790aed2d457cf6fcec75baff0c8a6
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-05-30 22:10:37 +0500

    change port

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const startServer = async () => {
   // Connect database
   await connectDB();
 
-  const port = config.port || 3000;
+  const port = config.port || 5000;
 
   app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
